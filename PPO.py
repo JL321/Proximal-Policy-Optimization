d33bfa067ec46406c525a703f241ea62bdf20861
JL321 <jliang@cluster12.ais.sandbox>
2019-11-11 17:29:02 -0500

    Changes prior to removing parameter storage

--- PPO.py
+++ PPO.py
@@ -15,27 +15,42 @@ def ffNetwork(x, action_dim, name = None):
 class PPO:
     
     #PPO- currently designed for a batch size of 1
-    def __init__(self, observation_dim, action_dim):
+    def __init__(self, observation_dim, action_dim, batch_size):
         
         self.sess = tf.Session()
         
         eps = 0.1
         self.x = tf.placeholder(dtype = tf.float32, shape = observation_dim)
         self.epsRewards = tf.placeholder(dtype = tf.float32, shape = None)        
-        self.adv = tf.placeholder(dtype = tf.float32, shape = None)
+        self.adv = tf.placeholder(dtype = tf.float32, shape = ())
+        self.batch_size = batch_size
         
         policyOut = ffNetwork(self.x, action_dim*2, name = 'CurrentPolicyNetwork')
         self.sampleOut = policyOut
         policyOld = ffNetwork(self.x, action_dim*2, name = 'OldPolicyNetwork')
 
-        self.policyOut = tfp.distributions.MultivariateNormalDiag(policyOut[0][:action_dim], policyOut[0][action_dim:]*tf.eye(action_dim)).sample()
-        self.policyOld = tfp.distributions.MultivariateNormalDiag(policyOld[0][:action_dim], policyOld[0][action_dim:]*tf.eye(action_dim)).sample()
+        print("Test: {}, {}".format(policyOut.get_shape()[0], policyOut.get_shape()))
 
+        print("Second test: {}, {}".format(policyOut[:, action_dim:].shape, policyOut.shape))
+        meanCurrent = policyOut[:, :action_dim]
+        stdCurrent = policyOut[:, action_dim:]
+        meanOld = policyOld[:, :action_dim]
+        stdOld = policyOld[:, action_dim:]
+
+        print("Shapes: {}, {}".format(meanCurrent.shape, stdCurrent.shape))
+        self.policyDist = tfp.distributions.MultivariateNormalDiag(meanCurrent, stdCurrent)
+        self.policyDistOld = tfp.distributions.MultivariateNormalDiag(meanOld, stdOld)
+    
+        self.policyOut = self.policyDist.sample()
+        self.policyOld = self.policyDistOld.sample()
+
+        print("Policy Sample Shape: {}".format(self.policyOut.shape))
         self.valueOut = ffNetwork(self.x, 1, name = 'ValueNetwork')
         
         bottomClip = (1-eps)*self.adv
         topClip = (1+eps)*self.adv
-        policyRatio = self.policyOut/self.policyOld
+        print(self.policyDist.log_prob(self.policyOut).shape)
+        policyRatio = self.policyDist.log_prob(self.policyOut)/self.policyDistOld.log_prob(self.policyOut)
         clipped_objective = tf.reduce_mean(tf.clip_by_value(policyRatio*self.adv, bottomClip, topClip))       
 
         self.valueObjective = tf.reduce_mean(tf.square(self.valueOut - self.epsRewards))
@@ -49,7 +64,7 @@ class PPO:
         print("Initialized Model")
 
     def predictPolicy(self, obs):
-        return self.sess.run(self.policyOut, feed_dict = {self.x: obs})
+        return self.sess.run(self.policyOut, feed_dict = {self.x: obs}), self.policyDist.log_prob(self.policyOut)
     
     def _predictValue(self, obs):
         return self.sess.run(self.valueOut, feed_dict = {self.x: obs})
@@ -90,14 +105,14 @@ class PPO:
             while endIdx < obs.shape[0]:
                 batchIdx = rdIdx[cIdx: endIdx]
                 #values per objective would need to be computed separately, and averaged outside of the main training graph (b, t_step, dim) doesn't work
-                self.sess.run(self.trainPolicy, feed_dict = {self.x: obs[batchIdx], self.adv: adv[batchIdx]})
+                self.sess.run(self.trainPolicy, feed_dict = {self.x: obs[batchIdx], self.adv: np.mean(adv[batchIdx])})
                 self.sess.run(self.trainValue, feed_dict = {self.x: obs[batchIdx], self.epsRewards: rewards[batchIdx]})
                 cIdx += mini_batch
                 endIdx += mini_batch
                 self.updateOldPolicy(currentPolicy)
                 
             batchIdx= rdIdx[cIdx:]
-            self.sess.run(self.trainPolicy, feed_dict = {self.x: obs[batchIdx], self.adv: adv[batchIdx]})
+            self.sess.run(self.trainPolicy, feed_dict = {self.x: obs[batchIdx], self.adv: np.mean(adv[batchIdx])})
             self.sess.run(self.trainValue, feed_dict = {self.x: obs[batchIdx], self.epsRewards: rewards[batchIdx]})
             self.updateOldPolicy(currentPolicy)
             np.random.shuffle(rdIdx)
--- ppoMain.py
+++ ppoMain.py
@@ -5,7 +5,7 @@ import numpy as np
 import tensorflow as tf
 
 tf.reset_default_graph()
-env = gym.make('Pendulum-v0')
+env = gym.make('HalfCheetah-v2')
 obs_space = env.observation_space.shape[0]
 action_space = env.action_space.shape[0]
 
@@ -20,16 +20,21 @@ def trainingLoop(model, episodes = 1000):
         epsReward = 0
         state_trajectory = []
         reward_trajectory = []
+        action_trajectory = []
+        logProb = []
         obs = env.reset()
         obs = np.expand_dims(obs, axis = 0)
         t = 0
         
-        while not done and t < 200:
-            action = model.predictPolicy(obs)
+        while not done and t < 1000:
+
+            action, log_prob = model.predictPolicy(obs)
             new_obs, reward, done, _ = env.step(action)
             new_obs = np.reshape(new_obs, (-1, obs_space))
             state_trajectory.append(obs) #We forego np.squeeze for batch size 1 operations
             reward_trajectory.append(reward)
+            action_trajectory.append(action)
+            logProb.append(log_prob)
             obs = new_obs
             epsReward += reward
             t += 1
@@ -37,24 +42,27 @@ def trainingLoop(model, episodes = 1000):
             #sample = model.return_sample(obs)  
             #print(sample)
 
-        model.trainingStep(reward_trajectory, state_trajectory)
+        model.trainingStep(reward_trajectory, state_trajectory, action_trajectory, logProb)
         reward_history.append(epsReward)
         done = False
     
-        if (i+1) % 50 == 0:
+        if (i+1) % 10 == 0:
             print("Last Action: {}".format(action))
-            print("Average of last 50 episodes: {}".format(np.mean(np.array(reward_history[-50:]))))
+            print("Average of last 10 episodes: {}".format(np.mean(np.array(reward_history[-10:]))))
             print("Episode Reward on Episode {}: {}".format(i, epsReward))
             print("Global Time Step: {}".format(global_t)) 
+
     plt.plot(reward_history)
     
 if __name__ == '__main__':
     
     if len(np.array(obs_space).shape) != 0:
+        print(obs_space.shape)
         state_space = (None, *obs_space)
     else:
         state_space = (None, obs_space)
-
-    model = PPO(state_space, action_space)
     
+    model = PPO(state_space, action_space, 16)
+    print("Action space: {}".format(action_space))
+
     trainingLoop(model)
